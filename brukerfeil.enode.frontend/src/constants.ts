eccaaa5738db1f6fa65627b6a7fd0c613d36b6ea
Gorm-Erik Aarsheim <gormerik96@gmail.com>
2020-04-06 12:35:13 +0200

    Updated frontend

--- brukerfeil.enode.frontend/src/constants.ts
+++ brukerfeil.enode.frontend/src/constants.ts
@@ -1,7 +1,24 @@
-export const BACKEND_BASEURL = 'https://localhost:5003'
+export const getBackendBaseUri = () => {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        return 'http://localhost:5003'
+    } else {
+        return process.env.BACKEND_URL
+    }
+}
+//"BackendURL": "https://cloud.elements-ecm.no/enode-service"
 
-export const MESSAGES_IN_ENDPOINT = '/messages/in'
+export const getFontendBaseUri = () => {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        return 'http://localhost:3000'
+    } else {
+        //Insert URL frontend will be hosted on
+        return ''
+    }
+}
+export const MESSAGES_IN_ENDPOINT = '/message/in'
 
-export const MESSAGES_OUT_ENDPOINT = '/messages/out'
+export const MESSAGES_OUT_ENDPOINT = '/message/out'
+
+export const MESSAGE_STATUS_ENDPOINT = '/messagestatus'
 
 export const ORGANIZATION_ENDPOINT = '/organizations'
